e711a090228412ca32cc803f8b9f33e4b54f3994
Shalini-CEG <ahana298@gmail.com>
2021-10-18 20:35:04 +0530

    Added icons

--- src/navigations/bottom-tabs.js
+++ src/navigations/bottom-tabs.js
@@ -1,6 +1,8 @@
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
 import React from "react";
-import { bgColor } from "../default/colors";
+import { bgColor, cherryRed } from "../default/colors";
 import AddToContacts from "../screens/AddToContacts";
 import LogOut from "../screens/LogOut";
 import ChatList from "../screens/ChatList";
@@ -11,14 +13,34 @@ export default function MyTabs({ checkUser, initComponent }) {
   return (
     <Tab.Navigator
       initialRouteName={initComponent}
-      activeColor={bgColor}
-      barStyle={{ backgroundColor: "black" }}
+      activeColor="white"
+      barStyle={{ backgroundColor: cherryRed }}
     >
-      <Tab.Screen name="ChatList" component={ChatList} />
-      <Tab.Screen name="New contact" component={AddToContacts} />
+      <Tab.Screen
+        name="ChatList"
+        component={ChatList}
+        options={{
+          tabBarLabel: "Contacts",
+          tabBarIcon: () => <Entypo name="chat" size={24} color={bgColor} />,
+        }}
+      />
+      <Tab.Screen
+        name="New contact"
+        component={AddToContacts}
+        options={{
+          tabBarLabel: "Add user",
+          tabBarIcon: () => (
+            <Ionicons name="person-add-sharp" size={24} color={bgColor} />
+          ),
+        }}
+      />
       <Tab.Screen
         name="Log Out"
         component={() => <LogOut checkUser={checkUser} />}
+        options={{
+          tabBarLabel: "Log out",
+          tabBarIcon: () => <Entypo name="log-out" size={24} color={bgColor} />,
+        }}
       />
     </Tab.Navigator>
   );
