bfbab8dccf757ee3c836059587bcf4d9593c8513
Shalini-CEG <ahana298@gmail.com>
2021-10-18 18:51:54 +0530

    Removed unwanted modules

--- src/screens/ChatList.js
+++ src/screens/ChatList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, Button, StyleSheet, FlatList } from "react-native";
+import { FlatList } from "react-native";
 import {
   Container,
   Card,
